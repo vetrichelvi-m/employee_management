3dbfab7ed027987d784e46928386534410a5a648
vetri <vetrichelvi26@gmail.com>
2024-04-26 21:49:13 +0530

    roles table

--- database/migrations/2024_04_26_153446_create_roles_table.php
+++ database/migrations/2024_04_26_153446_create_roles_table.php
@@ -13,7 +13,13 @@ return new class extends Migration
     {
         Schema::create('roles', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string('name',60);
+            $table->string('is_active',1)->default("1")->nullable();
+            $table->string('created_by',11);
+            $table->datetime('created_date');
+            $table->datetime('modified_date')->useCurrent()->nullable();
+            $table->string('modified_by',11)->nullable();
+            // $table->timestamps();
         });
     }
 
